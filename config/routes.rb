20bd7fb96b428fda98eb3398b153a7f7b01f4b72
Stephen Tong <stevetong83@hotmail.com>
2013-11-05 22:00:38 -0700

    boom

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,9 @@
 BetterEducation::Application.routes.draw do
-  devise_for :users
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
   root 'home#index'
 
+  devise_for :users
+
   resources :colleges
-
-
 
 end
